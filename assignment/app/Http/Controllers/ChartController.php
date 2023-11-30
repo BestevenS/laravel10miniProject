4996bdf1989904c2920882f8b9320760afa79501
Besteven <stefanostei18@gmail.com>
2023-11-30 22:46:23 +0200

    It's ready

--- assignment/app/Http/Controllers/ChartController.php
+++ assignment/app/Http/Controllers/ChartController.php
@@ -2,20 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\ChartData;
 
 class ChartController extends Controller
 {
+
     public function index()
     {
-        // Στατικά δεδομένα για το γράφημα
-        $chartData = [
-            ['name' => 'Προϊόν 1', 'data' => [1, 3, 4, 7, 2]],
-            ['name' => 'Προϊόν 2', 'data' => [5, 7, 6, 2, 8]],
-            // Μπορείτε να προσθέσετε περισσότερες σειρές δεδομένων εδώ
-        ];
+        $chartData = ChartData::all()->map(function ($data) {
+            return [
+                'name' => $data->name,
+                'data' => json_decode($data->value)
+            ];
+        });
 
-        // Επιστρέφουμε τα δεδομένα σε μορφή JSON
         return response()->json($chartData);
     }
 }
